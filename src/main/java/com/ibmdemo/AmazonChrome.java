79020828c503648a9e7774d68ef57ac2eb521067
Santhosh Veluri <saveluri@in.ibm.com>
2020-05-08 08:55:55 +0530

    added Extent reports

--- src/main/java/com/ibmdemo/AmazonChrome.java
+++ src/main/java/com/ibmdemo/AmazonChrome.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -24,4 +25,17 @@ public class AmazonChrome {
 		System.out.println("Amazon Page title is ======>>: " + title);
 		driver.quit();
 	}
+	
+	@Test
+	public void flipkartfirefox() {
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		String title = driver.getTitle();
+		System.out.println("Flipkart Page title is =====>>: " + title);
+		driver.quit();
+	}
+	
 }
